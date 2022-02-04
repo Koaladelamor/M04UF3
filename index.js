fec9ac625220913dba31fbee477a420bb13492be
enti <enti@enti.enti>
2022-02-04 14:34:17 +0100

    Juego con Phaser

---
+++ index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+
+let http = require("http");
+let fs = require("fs");
+
+console.log("Iniciando...");
+
+let http_server = http.createServer(function(req, res){
+	
+	if(req.url == "/character.png") {
+		
+		fs.readFile("character.png", function(error, data){
+			if (error) {
+				console.log("error");
+				return;
+			}
+
+			res.writeHead(200);
+			res.end(data);
+
+		});
+
+		return;
+
+	}
+
+	fs.readFile("index.html", function(error, data){
+		if (error) {
+			console.log("error");
+			return;
+		}
+		
+		res.writeHead(200);
+
+		res.end(data);
+		
+	});
+
+}).listen(1095);
